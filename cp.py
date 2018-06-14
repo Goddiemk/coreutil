837c77aba1b306ed493b2affc5ca107a56b8626f
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-14 14:55:17 +0630

    second

---
+++ cp.py
@@ -0,0 +1,11 @@
+import argparse,shutil
+
+parser=argparse.ArgumentParser(add_help=False)
+parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                        help='cp gnu utility in python')
+
+parser.add_argument('origin',help='specify the directory of the original location of file want to copy ')
+parser.add_argument('dest',help='specify the directory of the destination of file to be copied')
+
+args=parser.parse_args()
+shutil.copy(args.origin,args.dest)
