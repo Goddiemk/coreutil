a436e1ab8ecc2764886dc3c3720a20a4c0ce3aa9
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-15 15:49:09 +0630

    first

---
+++ wc.py
@@ -0,0 +1,17 @@
+import argparse, fileinput
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='wc gnu utility in python')
+parser.add_argument('path', help='specify a file with directory to display its text', nargs="+")
+args = parser.parse_args()
+
+try:
+    charcount = wordcount = 0
+    for line in fileinput.input():
+        linecount = fileinput.lineno()
+        wordcount += len(line.split(None))
+        charcount += len(line)
+    print("%d %d %d %s" % (linecount, wordcount, charcount, args.path))
+except IOError:
+    print("File %s not found" % args.path)
+    raise SystemExit
