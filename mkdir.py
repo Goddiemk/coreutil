44b8ae67ffb7023d5ae7f28a320acfb69855d8bb
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-19 10:06:14 +0630

    first

--- mkdir.py
+++ mkdir.py
@@ -1,10 +1,18 @@
 import argparse, pathlib
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                    help='dir gnu utility in python')
+parser.add_argument(
+    '-h',
+    '--help',
+    action='help',
+    default=argparse.SUPPRESS,
+    help='dir gnu utility in python')
 parser.add_argument('path', help='specify the directory want to be created')
-parser.add_argument('-p', '--parents', help='make parent directories as needed', action='store_true')
+parser.add_argument(
+    '-p',
+    '--parents',
+    help='make parent directories as needed',
+    action='store_true')
 args = parser.parse_args()
 
 try:
