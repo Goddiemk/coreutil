7bfd5230fd4f0a19ba72469f0acdd6cd3bc6d181
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-20 10:27:31 +0630

    gg

--- cat.py
+++ cat.py
@@ -1,9 +1,16 @@
 import argparse, fileinput
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                    help='cat gnu utility in python')
-parser.add_argument('path', help='specify a file with directory to display its text', nargs="+")
+parser.add_argument(
+    '-h',
+    '--help',
+    action='help',
+    default=argparse.SUPPRESS,
+    help='cat gnu utility in python')
+parser.add_argument(
+    'path',
+    help='specify a file with directory to display its text',
+    nargs="+")
 args = parser.parse_args()
 
 for line in fileinput.input():
